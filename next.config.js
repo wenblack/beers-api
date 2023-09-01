4e16ed86561988e533f99c171588e5bb5fc7612c
Wen Barbosa <wenblack012@gmail.com>
2023-09-01 08:46:12 -0300

    Created deploy actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output:'export'
+}
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = 'https://wenblack.github.io/'
+
+  assetPrefix = `${repo}/`
+  basePath = ``
 }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'http',
+              hostname: 'github.com',
+          },
+      ],
+  },
+  ...nextConfig
+}
